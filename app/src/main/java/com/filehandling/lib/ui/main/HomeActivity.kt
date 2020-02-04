d9429c01cc73d1bd61d9765f0b7482b93790e044
professional-lalit <lalit.appsmail@gmail.com>
2020-02-04 16:22:31 +0530

    file picker module completed 1

--- app/src/main/java/com/filehandling/lib/ui/main/HomeActivity.kt
+++ app/src/main/java/com/filehandling/lib/ui/main/HomeActivity.kt
@@ -67,6 +67,7 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    
     private fun initSections() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
